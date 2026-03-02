22308a890df81d04e13cdd0a772cfc9dad49a3dc
1feeling <forsersailes@gmail.com>
2026-03-02 22:39:02 +0500

    delete backend

--- Just Start/backend/routes/myths.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const mythController = require('../controllers/mythController');
-
-router.get('/', mythController.getAllMyths);
-router.get('/:id', mythController.getMythById);
-
-module.exports = router;
--- Just Start/backend/routes/roadmaps.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const roadmapController = require('../controllers/roadmapController');
-
-router.get('/', roadmapController.getAllRoadmaps);
-router.get('/:id', roadmapController.getRoadmapById);
-
-module.exports = router;
